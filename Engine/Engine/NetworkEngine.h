8ef84c5084242b1592aef4df5046d4a72e615f15
Alvin Philips <alvinphilips257@gmail.com>
2024-04-19 02:32:11 -0400

    Engine Silliness

--- Engine/Engine/NetworkEngine.h
+++ Engine/Engine/NetworkEngine.h
@@ -4,6 +4,7 @@
 #define _NETWORK_ENGINE_H_
 
 #include "NetworkCommon.h"
+#include "StandardTypes.h"
 
 class NetworkEngine final
 {
@@ -20,6 +21,10 @@ public:
 	bool IsClient() const { return is_client; }
 	bool IsServer() const { return is_server; }
 	void SendPacket(const RakNet::BitStream& bs) const;
+	unsigned short GetFriendCount() const { return friend_count; }
+	unsigned short GetMyIndex() const { return my_index; }
+	RakNet::RakNetGUID GetGUID() const { return me; } // Get gud lol
+	EventListener on_player_joined;
 private:
 	void Initialize(bool is_server);
 	void PreUpdate();
@@ -36,6 +41,9 @@ private:
 	RakNet::RakPeerInterface* rakInterface;
 	bool initialized = false;
 	std::vector<RakNet::RakNetGUID> connections;
+	unsigned short friend_count = 0;
+	unsigned short my_index = MAXSHORT;
+	RakNet::RakNetGUID me;
 	const unsigned int server_max_connections = 8;
 
 	friend class Engine;
