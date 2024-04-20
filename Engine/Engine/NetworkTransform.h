84a4555619e766476d5a505ca1988f0027ed9b94
Alvin Philips <alvinphilips257@gmail.com>
2024-04-19 23:04:52 -0400

    Revert "Added Rigidbodies and implemented NetworkTransforms"
    This reverts commit 1f532eb4f17416ff2d85712df21a5973741cda87.

--- Engine/Engine/NetworkTransform.h
+++ Engine/Engine/NetworkTransform.h
@@ -4,27 +4,15 @@
 #define NETWORK_TRANSFORM_H_
 
 #include "Component.h"
-#include "Transform.h"
-
-class Rigidbody;
 
 class NetworkTransform final : public Component
 {
     DECLARE_DYNAMIC_DERIVED_CLASS(NetworkTransform, Component);
 public:
-    void Serialize(RakNet::BitStream& bitStream) const override;
-    void Deserialize(RakNet::BitStream& bitStream) override;
-
-    void Initialize() override;
-    void Update() override;
-
-    Rigidbody* rb = nullptr;
-    Vec2 server_position;
-    Vec2 server_velocity;
-    Vec2 data; // For the server, this is: server sync delay | server sync timer
-
-    void SendSyncRpc() const;
-    void RpcSyncNetworkTransform(RakNet::BitStream& bitStream);
+    Vec2 prev_position;
+    Vec2 prev_velocity;
+    Vec2 target_position;
+    Vec2 target_velocity;
 };
 
 #endif
